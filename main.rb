c4df9559015a468c8b5e458e1de396d4bd81b8b9
Riley <169967357+CatdadR@users.noreply.github.com>
2024-08-16 13:11:00 +1000

    Removed redundant code

--- main.rb
+++ main.rb
@@ -1,25 +1,8 @@
-require_relative 'lib/event_manager'
-require_relative 'lib/game_handler'
-require_relative 'lib/display'
-require_relative 'lib/game_instance'
+require_relative 'lib/hangman.rb'
+
 
 def hang_man
-  event_manager = EventManager.new
-  game = GameHandler.new
-  display = Display.new
-  gameinstance = GameInstance.new
-
-  display.print('Hangman')
-  display.print('1) New Game')
-  display.print('2) Load Game')
-  selection = gets.chomp  
-  if selection == '1' 
-    game.new_game(event_manager, display)
-    else if selection == '2'
-     exit
-    end  
-  end 
+  Hangman.new
 end
-
 
 hang_man
